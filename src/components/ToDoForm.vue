1e9948f42252d5062d88c9001847bfcf0039a5b7
TheGuyTheClasher <nishikant.bawache.nb@gmail.com>
2022-02-14 16:08:03 +0530

    Added todo-components and styling

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <form>
+        <label class="label__lg" for="new-todo-input">
+            What needs to be done?
+        </label>
+        <input class="input__lg" type="text" v-model.lazy.trim="label" id="new-todo-input" autocomplete="off" name="new-todo">
+        <button class="btn btn__primary btn__lg" @click.prevent="onSubmit" type="submit">
+            Add
+        </button>
+    </form>
+</template>
+
+<script>
+export default {
+    methods: {
+        onSubmit(){
+            if(this.label === ''){
+                return
+            }
+            // passing the data to the parent i.e. app.vue by emmiting an event
+            this.$emit("todo-added", this.label);
+            this.label = '';
+        }
+    },
+    data(){
+        return{
+            label: ''
+        }
+    }
+}
+</script>
